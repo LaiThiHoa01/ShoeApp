a99012034381297f00543e3f08769cbb8e3813e6
ThanVanDanhh <23130043@st.hcmuaf.edu.vn>
2026-05-30 15:59:46 +0700

    checkout view

--- app/src/main/java/com/example/shoeapp/CheckoutActivity.java
+++ app/src/main/java/com/example/shoeapp/CheckoutActivity.java
@@ -3,15 +3,15 @@ package com.example.shoeapp;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class CartActivity extends BaseSoleStepActivity {
+public class CheckoutActivity extends BaseSoleStepActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
+        setContentView(R.layout.activity_checkout);
         setupScreen(BottomNavHelper.TAG_CART);
 
-        findViewById(R.id.checkout_button).setOnClickListener(v ->
-                startActivity(new Intent(this, CheckoutActivity.class)));
+        findViewById(R.id.place_order_button).setOnClickListener(v ->
+                startActivity(new Intent(this, QRPaymentActivity.class)));
     }
 }
